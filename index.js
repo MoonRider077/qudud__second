2cd429ebb26af134f7b58a2c81f90d2e5bf1a1ff
Yusufjon Muxammadjonov <moonrider@Yusufjons-MacBook-Pro.local>
2024-06-21 12:02:29 +0500

    fixed

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     accordionButtons.forEach(button => {
         button.addEventListener('click', function() {
-            const plusButton = this.querySelector('.plus__button p');
+            const plusButton = this.querySelector('.plus__button img');
 
             accordionButtons.forEach(btn => {
                 if (btn !== this) {
                     btn.classList.add('collapsed');
-                    const otherPlusButton = btn.querySelector('.plus__button p');
-                    otherPlusButton.textContent = '+';
+                    const otherPlusButton = btn.querySelector('.plus__button img');
+                    otherPlusButton.src = './assets/icons/Plus.svg';
                 }
             });
 
             if (this.classList.contains('collapsed')) {
                 this.classList.add('collapsed');
-                plusButton.textContent = '+';
+                plusButton.src = './assets/icons/Plus.svg';
             } else {
                 this.classList.remove('collapsed');
-                plusButton.textContent = '-';
+                plusButton.src = './assets/icons/Minus.svg';
             }
         });
     });
 });
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
